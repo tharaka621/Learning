86ad04f8ebdc6598b8074d9b4aa11a7cf2261400
tharaka621 <j.a.d.tharaka@gmail.com>
2021-01-30 21:07:33 +0530

    Array Two Dimension basic

---
+++ src/array/twoDimensionArray.java
@@ -0,0 +1,24 @@
+package array;
+
+public class twoDimensionArray {
+    public static void main(String[] args) {
+        int twoDArray[][] = new int[3][2];
+
+        System.out.println("number of rows: "+twoDArray.length); //number of rows
+        System.out.println("number of columns: "+twoDArray[0].length); //length of the 1st row ( Number of columns)
+
+        for(int i=0;i<=twoDArray.length-1;i++){  //adding values
+            for(int j=0;j<=twoDArray[i].length-1;j++){
+                twoDArray[i][j]=i;
+            }
+        }
+
+        for(int i=0;i<=twoDArray.length-1;i++){  //Displaying Values
+            for(int j=0;j<=twoDArray[i].length-1;j++){
+                System.out.print(twoDArray[i][j]+" ");
+            }
+            System.out.println("");
+        }
+    }
+
+}
