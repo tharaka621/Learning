9011e99d0be8125d3ae0c3802bedea1f6ee0d75f
tharaka621 <j.a.d.tharaka@gmail.com>
2021-02-02 06:28:19 +0530

    Contructor Overloading

--- src/ConstructorDemo/ConstructorDemo.java
+++ src/ConstructorDemo/ConstructorDemo.java
@@ -2,15 +2,15 @@ package ConstructorDemo;
 
 public class ConstructorDemo {
 
-    int x,y;
+    int x,y,z;
 
-    /*
+
     ConstructorDemo(){ //default Constructor **********
         x =100;
         y=200;
         System.out.println(x+y);
     }
-     */
+
 
     ConstructorDemo(int a, int b){ //Parameterized Constructor **********
         x =a;
@@ -18,10 +18,23 @@ public class ConstructorDemo {
         System.out.println(x+y);
     }
 
+    ConstructorDemo(int a, int b,int c){ //Parameterized Constructor with different parameters **********
+        x =a;
+        y=b;
+        z=c;
+        System.out.println(x+y+z);
+    }
+
     public static void main(String[] args) {
 
-        //ConstructorDemo exampleConstructor1 = new ConstructorDemo(); //for default constructor
+        ConstructorDemo exampleConstructor1 = new ConstructorDemo(); //for default constructor
         ConstructorDemo exampleConstructor2 = new ConstructorDemo(123,432); //for Parameterized constructor
-
+        ConstructorDemo exampleConstructor3 = new ConstructorDemo(123,432,34);
     }
 }
+
+/*
+Overloading Constructor : Same Constructor is using in the same class
+*******can make several Constructors. But the Constructor parameters or data types should be different.
+******* Cant make two DEFAULT CONSTRUCTOR
+ */
