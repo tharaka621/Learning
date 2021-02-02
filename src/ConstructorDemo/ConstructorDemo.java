a6fe86ebe83239bf056690b06e1222f8f03edb3f
tharaka621 <j.a.d.tharaka@gmail.com>
2021-02-02 06:21:57 +0530

    Constructor demo Parameterized Constructor

--- src/ConstructorDemo/ConstructorDemo.java
+++ src/ConstructorDemo/ConstructorDemo.java
@@ -4,16 +4,24 @@ public class ConstructorDemo {
 
     int x,y;
 
+    /*
     ConstructorDemo(){ //default Constructor **********
         x =100;
         y=200;
         System.out.println(x+y);
     }
+     */
 
+    ConstructorDemo(int a, int b){ //Parameterized Constructor **********
+        x =a;
+        y=b;
+        System.out.println(x+y);
+    }
 
     public static void main(String[] args) {
 
-        ConstructorDemo exampleConstructor1 = new ConstructorDemo();
+        //ConstructorDemo exampleConstructor1 = new ConstructorDemo(); //for default constructor
+        ConstructorDemo exampleConstructor2 = new ConstructorDemo(123,432); //for Parameterized constructor
 
     }
 }
