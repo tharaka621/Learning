b453768cdc0ed312faa0203467154806c90556d3
tharaka621 <j.a.d.tharaka@gmail.com>
2021-02-03 06:40:51 +0530

    Inheritance basic Introduction

---
+++ src/OOP_Inheritance/InheritanceDemo.java
@@ -0,0 +1,21 @@
+package OOP_Inheritance;
+
+/*
+Using one Class all properties from a another class
+Parent Class >>>>>>>> Child Class
+to do this use 'extend' key word
+
+there are five types of inheritance
+    1.Single       : 1 parent , 1 child (A to B)
+    2.Multilevel   : 1 parent , 1 child , 1 child (A to B) (B to C)
+    3.Hierarchical : 1 Parent , several child (A to B , A to C)
+    4.Multiple     : several parent, 1 child  (A to C , B to C)
+    5.Hybrid       : Hierarchical + Multiple : (A to B , A to C) (B to D, C to D)
+
+    1 , 2 & 3 types can be make using "class" concept (1 class can not extend several parent classes)
+    4 & 5 types can be make using "interface" concept
+
+ */
+
+public class InheritanceDemo {
+}
