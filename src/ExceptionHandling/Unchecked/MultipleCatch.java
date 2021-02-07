2adcdf12f76aae219db7ec400fa257fe7ab6775b
tharaka621 <j.a.d.tharaka@gmail.com>
2021-02-07 05:48:57 +0530

    Multiple Catch to Exception

---
+++ src/ExceptionHandling/Unchecked/MultipleCatch.java
@@ -0,0 +1,29 @@
+package ExceptionHandling.Unchecked;
+
+import java.util.Scanner;
+
+public class MultipleCatch {
+    public static void main(String[] args) {
+        System.out.println("Program is starting........");
+        System.out.println("Program is in progress................");
+
+
+        int a =100;
+
+        try{
+           // System.out.println(100/0);  // 100/0 >> ArithmeticException **********************
+
+            String s = "Welcome";
+            int n = Integer.parseInt(s);  //Number Format Exception **************
+
+        }catch(ArithmeticException x){ //x is an any variable
+            System.out.println("Arithmetic Exception is occurred...... and Handle Successfully.... ");
+        }catch(NullPointerException x){ //x is an any variable
+            System.out.println("Null Pointer Exception is occurred...... and Handle Successfully.... ");
+        }catch(NumberFormatException x){ //x is an any variable
+            System.out.println("Number Format Exception is occurred...... and Handle Successfully.... ");
+        }
+
+        System.out.println("Program Complete .............");
+    }
+}
