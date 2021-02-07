706288f9ae10571cd7fdc886a9c388b45703e66b
tharaka621 <j.a.d.tharaka@gmail.com>
2021-02-07 06:33:48 +0530

    Throw comment added

--- src/ExceptionHandling/Checked/Throws.java
+++ src/ExceptionHandling/Checked/Throws.java
@@ -1,5 +1,6 @@
 package ExceptionHandling.Checked;
 
+//Throw is for only Checked Exception 
 public class Throws {
     public static void main(String[] args) throws InterruptedException{  //Throws == Method level handling exception
         System.out.println("Program is starting........");
